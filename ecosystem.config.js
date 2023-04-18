73a27a1b6821cc089d79c3a5a8f4b93edc71aad1
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-18 17:34:30 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     apps : [{
       name   : "express_web_server",
-      cwd    : "/home/ubuntu/pm2-apps/express-web-server",
       script : "app.js",
       env    : {
         "EXPRESS_PORT" : 3011
