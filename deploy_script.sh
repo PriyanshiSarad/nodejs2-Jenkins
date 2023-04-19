1552c5433ee62da36c6fee18aaa1ea483f17f53f
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-19 08:50:04 +0530

    changes

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,7 +4,7 @@ REMOTE_PATH=/home/ubuntu/nodejs2-Jenkins
 ssh ubuntu@172.31.38.201 <<EOF
 cd /home/ubuntu/nodejs2-Jenkins
 rm -rf *
-scp -r /var/lib/jenkins/workspace/nodeApp/* $REMOTE_SERVER:$REMOTE_PATH
+scp -r /var/lib/jenkins/workspace/nodeApp/*.json $REMOTE_SERVER:$REMOTE_PATH
 npm install --production
 pm2 restart ecosystem.config.js
 exit
