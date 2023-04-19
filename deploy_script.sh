8c98b4c0e490fbc903b5d62b3f53d53d2c446ee7
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-19 15:25:30 +0530

    changes

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,6 +1,13 @@
-ssh ubuntu@172.31.38.201 <<EOF
+#!/bin/bash
+PROCESS_NAME="express_web_server"
+ECOSYSTEM_FILE="/home/ubuntu/nodejs2-Jenkins"
 cd /home/ubuntu/nodejs2-Jenkins
 npm install --production
-pm2 restart ecosystem.config.js
-exit
-EOF
+pm2 delete $PROCESS_NAME
+
+while pm2 list $PROCESS_NAME|grep express_web_server|awk '{print $18}'; do
+    sleep 1
+done
+
+pm2 start $ECOSYSTEM_FILE --name $PROCESS_NAME --wait-ready --listen-timeout 5000
+
