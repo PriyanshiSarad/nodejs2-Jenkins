7ae0abd5ffb3397bcc8ccc96ce5ff73e0d589f04
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-19 15:37:34 +0530

    changes

--- deploy_script.sh
+++ deploy_script.sh
@@ -5,7 +5,7 @@ cd /home/ubuntu/nodejs2-Jenkins
 npm install --production
 pm2 delete $PROCESS_NAME
 
-while pm2 list $PROCESS_NAME|grep express_web_server|awk '{print $18}'; do
+while pm2 list|grep $PROCESS_NAME|awk '{print $18}'|grep -q 'online'; do
     sleep 1
 done
 
