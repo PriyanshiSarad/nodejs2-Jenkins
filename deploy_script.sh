9d22993b8f113e442d419c3fc6a1a7cb36e6b10e
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-19 16:04:18 +0530

    changes

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,9 +4,5 @@ ECOSYSTEM_FILE="/home/ubuntu/nodejs2-Jenkins"
 cd /home/ubuntu/nodejs2-Jenkins
 npm install --production
 pm2 delete $PROCESS_NAME
-while pm2 list|grep $PROCESS_NAME|awk '{print $18}'|grep 'online'; do
-    sleep 1
-done
-pm2 start $ECOSYSTEM_FILE --name $PROCESS_NAME --wait-ready --listen-timeout 5000
 exit
 EOF
