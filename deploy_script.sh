46be6cf2085d47d2ed6764200c13c2a828026a4d
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-19 16:33:12 +0530

    changes

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,6 +3,7 @@ ECOSYSTEM_FILE="/home/ubuntu/nodejs2-Jenkins"
 cd /home/ubuntu/nodejs2-Jenkins
 npm install --production
 pm2 delete $PROCESS_NAME
+pm2 save --force
 while pm2 list|grep $PROCESS_NAME|awk '{print $18}'|grep 'online'; do
     sleep 1
 done
