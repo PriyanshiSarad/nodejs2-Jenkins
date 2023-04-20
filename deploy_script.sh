cf2e83ec423b8dde78e0662e3178b60020636b95
Priyanshi Sarad <itspriyanshisarad@gmail.com>
2023-04-20 20:08:13 +0530

    changes

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,9 +1,6 @@
 PROCESS_NAME="express_web_server"
 ECOSYSTEM_FILE="/home/ubuntu/nodejs2-Jenkins/ecosystem.config.js"
-cd /home/ubuntu/nodejs2-Jenkins
-npm install --production
 pm2 delete $PROCESS_NAME
 pm2 save --force
 pm2 start $ECOSYSTEM_FILE --name $PROCESS_NAME
 pm2 save --force
-pm2 status $PROCESS_NAME
